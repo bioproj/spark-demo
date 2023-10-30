b0cc6a2c87ce5024e969c5fcf8a9d54494482dbe
wangyang <1749748955@qq.com>
2023-10-30 21:53:48 +0800

    update

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,2 +1,51 @@
+
+
+import com.google.common.base.Splitter;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class WordCount {
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setAppName("WordCount").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+        JavaRDD<String> lines = sc.textFile("file:/home/wy/workspace/spark-demo/workDir/input.txt");
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+
+            @Override
+            public Iterator<String> call(String line) throws Exception {
+                return Arrays.asList(line.split(" ")).iterator();
+            }
+        });
+
+        JavaPairRDD<String,Integer> pairRDD = words.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String word) throws Exception {
+                return new Tuple2<String, Integer>(word,1);
+            }
+        });
+
+        JavaPairRDD<String,Integer> wordCounts = pairRDD.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+
+        wordCounts.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+            @Override
+            public void call(Tuple2<String, Integer> wordcount) throws Exception {
+                System.out.println(wordcount._1 + ":"+wordcount._2);
+            }
+        });
+    }
 }
